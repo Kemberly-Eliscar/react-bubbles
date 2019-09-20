9783bef89773097de46a58ab6cb59e831680a930
Kemberly Eliscar <kemberlyeliscar@Kemberlys-Air-3.home>
2019-09-20 13:10:46 -0400

    built login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useEffect } from 'react'
+import { Form , Field, withFormik } from 'formik'
+import * as Yup from 'yup'
+import { connect } from 'react-redux'
+import { logInAttempt } from '../actions'
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+const LoginForm = props => {
 
-export default Login;
+    useEffect(()=> {
+        if(props.hasToken && !props.isLoggingIn){
+            props.history.push('/bubbles')
+        }
+    })
+
+    return(
+        <Form>
+        <h2>React-Bubbles Login</h2>
+            <Field name = 'username' type = 'text' placeholder = 'Username. . .'/>
+            <Field name = 'password' type = 'password' placeholder = 'Password. . .'/>
+            <button type = 'submit'>Log In</button>
+        </Form>
+    )
+}
+
+const FormikLogin = withFormik({
+    mapPropsToValues({username, password}){
+        return{
+            username: username || '',
+            password: password || ''
+        }
+    },
+    validationSchema: Yup.object().shape({
+        username: Yup.string().required('Enter your username to continue'),
+        password: Yup.string().required('Enter your password to continue')
+    }),
+    handleSubmit(values, props){
+        const dataToPost = {
+            username: values.username,
+            password: values.password
+        }
+        props.props.logInAttempt(dataToPost)
+
+    }
+})(LoginForm)
+
+const mapStateToProps = state => {
+    return {
+        isLoggingIn: state.isLoggingIn,
+        hasToken: state.hasToken
+    }
+}
+
+export default connect(mapStateToProps, { logInAttempt })(FormikLogin)
